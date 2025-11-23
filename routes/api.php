a49620ac2a14cd626953c7ab114e43dfebda6f19
Kelly Isabel Altamirano Abata <kialtamirano1@espe.edu.ec>
2025-11-23 17:26:44 -0500

    Remove commented test route from api.php
    Removed commented test route for user.

--- routes/api.php
+++ routes/api.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 
-// Ruta de prueba del usuario (opcional)
+
 Route::get('/user', function (Request $request) {
     return $request->user();
 
