591888b4bb257ab8240a97aa0d97f48e22f364dc
Kelly Isabel Altamirano Abata <kialtamirano1@espe.edu.ec>
2025-11-23 20:54:11 -0500

    Actulizado api.php funcional
    Removed user route and consolidated post routes under auth.micro middleware.

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,15 @@
-
 <?php
 
 use App\Http\Controllers\PostController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+// Todas las rutas del microservicio de Posts protegidas con auth.micro
+Route::middleware('auth.micro')->group(function () {
 
+    Route::get('/posts', [PostController::class, 'index']);      // Listar posts
+    Route::get('/posts/{id}', [PostController::class, 'show']); // Ver un post
+    Route::post('/posts', [PostController::class, 'store']);    // Crear post
+    Route::put('/posts/{id}', [PostController::class, 'update']); // Actualizar
+    Route::delete('/posts/{id}', [PostController::class, 'destroy']); // Eliminar
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-
-})->middleware('auth:sanctum');
-
-    Route::get('/posts', [PostController::class, 'index'])->middleware('auth.micro');    // Listar posts
-    Route::get('/posts/{id}', [PostController::class, 'show']);  // Ver un post
-    Route::post('/posts', [PostController::class, 'store']);     // Crear post
-    Route::put('/posts/{id}', [PostController::class, 'update']); // Actualizar post
-    Route::delete('/posts/{id}', [PostController::class, 'destroy']); // Eliminar post
+});
